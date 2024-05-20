c131cd44fc4258a3fa305b0cabd2896c68f1f9ce
Francovg18 <153469619+Francovg18@users.noreply.github.com>
2024-05-20 17:33:09 -0400

    Update Pregunta1.py

--- Pregunta1.py
+++ Pregunta1.py
@@ -1,15 +1,3 @@
-'''
-1. Seleccione un dataset de datos tabulares (UCI, KAAGLE). Realice lo siguiente:
-
-   a. Con Python sin uso de librerías, calcule del ultimo cuartil, percentil 80 por columna; explique qué significa en cada caso.
-
-   b. Realice lo mismo del inciso (a) con el uso de numpy y pandas
-
-   c. Obtenga la media, mediana, moda y geométrica; explique la diferencia de los resultados y cuál de ellas se puede utilizar en un artículo científico.
-
-   d. Grafique los datos y explique su comportamiento (PYTHON)
-'''
-
 import numpy as np
 from collections import Counter
 import pandas as pd
@@ -17,7 +5,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import csv
 
-# Cargar datos
 datos = []
 ruta_archivo = "C:/Users/Alex Choque/Downloads/iris.csv"
 
@@ -25,17 +12,7 @@ with open(ruta_archivo, newline='') as csvfile:
     lector = csv.reader(csvfile)
     for fila in lector:
         datos.append(fila)
-
-# Mostrar las primeras filas del archivo
-num_filas_mostrar = 5  
-
-for fila in datos[:num_filas_mostrar]:
-    print(fila)
-
-print("****************************")
-
 # Inciso a (sin usar librerías)
-# Convertir columnas numéricas a flotantes
 def convertir_a_flotante(columna):
     columna_convertida = []
     for valor in columna:
@@ -43,7 +20,7 @@ def convertir_a_flotante(columna):
             valor_convertido = float(valor)
             columna_convertida.append(valor_convertido)
         except ValueError:
-            pass #igno flotantes
+            pass 
     return columna_convertida
 
 # Convertir las columnas numéricas a flotantes
@@ -76,25 +53,19 @@ for columna, valores in resultados.items():
     print()
 
 # Inciso B   
-print("Inciso B")
-# Convertir a DataFrame
 df = pd.DataFrame(datos, columns=['largo_sépalo', 'ancho_sépalo', 'largo_pétalo', 'ancho_pétalo', 'especie'])
-
-# Convertir columnas numéricas a flotantes
 columnas_numericas = df.iloc[:, :-1].apply(pd.to_numeric, errors='coerce')
 
 # Calcular el último cuartil y el percentil 80 usando Pandas
 ultimo_cuartil = columnas_numericas.quantile(0.75)
 percentil_80 = columnas_numericas.quantile(0.8)
 
-# Imprimir los resultados
 print("Último Cuartil:")
 print(ultimo_cuartil)
 print("\nPercentil 80:")
 print(percentil_80)
 
 # Inciso C
-print("Inciso C")
 datos_flotantes = []
 for fila in datos:
     try:
@@ -117,16 +88,12 @@ moda = [Counter(columna).most_common(1)[0][0] for columna in datos_transpuestos]
 # Calcular la media geométrica
 media_geométrica = [np.prod(columna)**(1/len(columna)) for columna in datos_transpuestos]
 
-# Imprimir resultados
 print("Media:", media)
 print("Mediana:", mediana)
 print("Moda:", moda)
 print("Media Geométrica:", media_geométrica)
 
 # Inciso D
-print("Inciso D")
-
-# Convertir a DataFrame de Pandas
 df = pd.DataFrame(datos, columns=['largo_sépalo', 'ancho_sépalo', 'largo_pétalo', 'ancho_pétalo', 'especie'])
 
 # Convertir columnas numéricas a flotantes sin incluir la primera fila
@@ -134,8 +101,6 @@ columnas_numericas = df.iloc[1:, :-1].apply(pd.to_numeric, errors='coerce')
 
 # Gráfico de dispersión usando Seaborn
 sns.pairplot(columnas_numericas, markers=['o', 's', 'D'])
-
-# Mostrar el gráfico
 plt.show()
 
 
